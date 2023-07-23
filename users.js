1bf7efcfae9b9aba73d431f6a928939fa269c426
Angelo <122828995+allconditionguy@users.noreply.github.com>
2023-07-23 15:12:19 +0100

    Add files via upload

---
+++ users.js
@@ -0,0 +1,26 @@
+export const USERS = [
+  { 
+    user: 'nba',
+    image: 'https://th.bing.com/th/id/R.614f446b725194fc51dc2ccc5e88ecc8?rik=B9cVCWKMNQUtvA&pid=ImgRaw&r=0',
+  },
+  {
+    user: 'leomessi',
+    image: 'https://th.bing.com/th/id/OIP.3keMZAjhY4H4F2Eh_6Ye3QHaHa?pid=ImgDet&rs=1',
+  },
+  {
+    user: '_angelo.d',
+    image: 'https://www.llsvisionaries.org/sites/mwoy/files/styles/participant_image/public/participant/user-placeholder_0.png?itok=KFiQTq89',
+  },
+  {
+    user: 'kromthrift',
+    image: 'https://th.bing.com/th/id/OIP.vnjQfLVveNyy38wwavG8kQHaHa?pid=ImgDet&rs=1',
+  },
+  {
+    user: 'poole',
+    image: 'https://th.bing.com/th/id/OIP.W6wBLHBob8_3bQWyNgfbhwHaEK?pid=ImgDet&rs=1',
+  },
+  {
+    user: 'bleacherreport',
+    image: 'https://th.bing.com/th/id/R.18717c1259971dbf3d0f3877c5ad3e2b?rik=Zx2t24Z6rKcBOw&pid=ImgRaw&r=0',
+  },
+]
